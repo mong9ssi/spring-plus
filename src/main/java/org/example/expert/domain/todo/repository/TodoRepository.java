4e8a3198b1b742eaa033e7b530ff9f81fd9316e6
mong9ssi <mong9ssi@naver.com>
2025-06-25 19:44:19 +0900

    Lv3

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -18,4 +20,17 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    @Query("""
+        SELECT t FROM Todo t
+        WHERE (:weather IS NULL OR t.weather = :weather)
+        AND (:start IS NULL OR t.modifiedAt >= :start)
+        AND (:end IS NULL OR t.modifiedAt <= :end)
+    """)
+    Page<Todo> searchTodos(
+            @Param("weather") String weather,
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            Pageable pageable
+            );
 }
